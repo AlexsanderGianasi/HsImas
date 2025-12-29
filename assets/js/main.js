7c6713837df6229703d10ef7ba4fe26cabbcc177
Alexsander Gianasi <206681800+AlexsanderGianasi@users.noreply.github.com>
2025-12-29 19:28:19 -0300

    comit

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,3 +26,44 @@ window.addEventListener("scroll", function(){
   let header = document.querySelector('#header')
   header.classList.toggle('roll', window.scrollY > 0)
 })
+let currentSection = 0;
+const sections = document.querySelectorAll("section");
+const footer = document.querySelector("footer");
+let isScrolling = false;
+
+window.addEventListener("wheel", (e) => {
+  e.preventDefault();
+
+  if (isScrolling) return;
+  isScrolling = true;
+
+
+  if (e.deltaY > 0) {
+    if (currentSection < sections.length - 1) {
+      currentSection++;
+      sections[currentSection].scrollIntoView({ behavior: "smooth" });
+    } else {
+    
+      document.body.style.overflow = "auto";
+      window.scrollTo({
+        top: document.body.scrollHeight,
+        behavior: "smooth"
+      });
+    }
+  }
+
+
+  else {
+    if (window.scrollY > sections[sections.length - 1].offsetTop) {
+    
+      sections[sections.length - 1].scrollIntoView({ behavior: "smooth" });
+    } else if (currentSection > 0) {
+      currentSection--;
+      sections[currentSection].scrollIntoView({ behavior: "smooth" });
+    }
+  }
+
+  setTimeout(() => {
+    isScrolling = false;
+  }, 800);
+}, { passive: false });
